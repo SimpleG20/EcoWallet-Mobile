022d18a4e2afc9dabe40f685001ac6c8fba6abb4
Arthur Gonçalves <arthurga54@gmail.com>
2026-07-10 11:17:05 -0300

    07-10 refactor: convert auth module to package imports

--- lib/features/auth/domain/usecases/auth_usecases.dart
+++ lib/features/auth/domain/usecases/auth_usecases.dart
@@ -1,6 +1,6 @@
 library;
 
-export 'sign_in.dart';
-export 'sign_up.dart';
-export 'log_out.dart';
-export 'check_auth_status.dart';
+export 'package:eco_wallet/features/auth/domain/usecases/sign_in.dart';
+export 'package:eco_wallet/features/auth/domain/usecases/sign_up.dart';
+export 'package:eco_wallet/features/auth/domain/usecases/log_out.dart';
+export 'package:eco_wallet/features/auth/domain/usecases/check_auth_status.dart';
--- lib/features/auth/domain/usecases/check_auth_status.dart
+++ lib/features/auth/domain/usecases/check_auth_status.dart
@@ -1,9 +1,9 @@
 import 'package:fpdart/fpdart.dart';
 
-import '../../../../core/errors/base_failure.dart';
-import '../../../../core/usecases/base_usecase.dart';
-import '../../../user/domain/entities/user.dart';
-import '../repositories/base_auth_repository.dart';
+import 'package:eco_wallet/core/errors/base_failure.dart';
+import 'package:eco_wallet/core/usecases/base_usecase.dart';
+import 'package:eco_wallet/features/user/domain/entities/user.dart';
+import 'package:eco_wallet/features/auth/domain/repositories/base_auth_repository.dart';
 
 class CheckAuthStatus implements BaseUsecase<User, NoParams> {
   final BaseAuthRepository repository;
